1400d93268fff1288ce9e7236446ce516d2b190f
Circe Grand <circegrand@gmail.com>
2020-04-02 17:59:00 +0200

    :fire: #22 remove useless interface in room store

--- src/redux/Room/index.ts
+++ src/redux/Room/index.ts
@@ -7,16 +7,6 @@ export interface RoomState {
   id: string | null;
   error: number | null;
   code: number | null;
-  players: Player[];
-}
-
-export interface Player {
-  id: string | null;
-  position?: number | null;
-  deviceSize: {
-    width: number | null;
-    height: number | null;
-  };
 }
 
 export const slice = createSlice({
@@ -50,12 +40,6 @@ export const slice = createSlice({
       action: PayloadAction<PayloadsRoom.JoinError>
     ) => {
       state.code = action.payload.code;
-    },
-    addPlayer: (
-      state: RoomState,
-      action: PayloadAction<{ amount: number }>
-    ) => {
-      console.log("add player");
     }
   }
 });
@@ -66,12 +50,10 @@ const getRoot = (state: RootState) => state.room;
 const selectId = (state: RootState) => getRoot(state).id;
 const selectError = (state: RootState) => getRoot(state).error;
 const selectCode = (state: RootState) => getRoot(state).code;
-const selectPlayers = (state: RootState) => getRoot(state).players;
 export const selectors = {
   selectId,
   selectError,
-  selectCode,
-  selectPlayers
+  selectCode
 };
 
 // reducer / actions
