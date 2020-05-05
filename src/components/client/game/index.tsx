30a31b87b1b992064011317a066e1c534200cfb3
Alexandre Massé <alexandre.masse94@gmail.com>
2020-05-05 02:35:06 +0200

    :alembic: #31 Game : add experiment with drag & drop

--- src/components/client/game/index.tsx
+++ src/components/client/game/index.tsx
@@ -1,29 +1,81 @@
-import React, { FC } from "react";
+import React, { FC, useCallback, useEffect, useState, useRef } from "react";
 import Area from "../../Area";
+
+import { gsap, Linear } from "gsap";
+import { Draggable } from "gsap/Draggable";
 
 import "./game.scss";
 import { useTypedSelector } from "../../../redux/store";
-import { selectors } from "../../../redux";
+import { selectors, actions } from "../../../redux";
+import { useDispatch } from "react-redux";
+
+gsap.registerPlugin(Draggable);
 
 const Game: FC = () => {
+  const dispatch = useDispatch();
+
   // selectors
   const gameTick = useTypedSelector(selectors.game.selectTick);
   const gamePosition = useTypedSelector(selectors.game.selectPosition);
+
+  // ref
+  const $circle = useRef(null);
+  const $draggable = useRef<Draggable[] | null>(null);
+
+  // state
+
+  const [isDragging, setIsDragging] = useState(false);
+
+  // handlers
+
+  const handleOnDragStart = useCallback(() => {
+    setIsDragging(true);
+  }, []);
+
+  const handleOnDragEnd = useCallback(() => {
+    setIsDragging(false);
+  }, []);
+
+  const handleOnDrag = useCallback(() => {
+    if ($draggable.current) {
+      const { x, y } = $draggable.current[0];
+
+      dispatch(
+        actions.webSocket.emit.game.positionUpdate({
+          data: { x, y }
+        })
+      );
+    }
+  }, [dispatch]);
+
+  // effects
+
+  useEffect(() => {
+    $draggable.current = Draggable.create($circle.current, {
+      bounds: ".area",
+      onDragStart: handleOnDragStart,
+      onDragEnd: handleOnDragEnd,
+      onDrag: handleOnDrag
+    });
+  }, [handleOnDrag, handleOnDragEnd, handleOnDragStart]);
+
+  useEffect(() => {
+    if (!isDragging) {
+      gsap.to($circle.current, {
+        x: gamePosition.x,
+        y: gamePosition.y,
+        duration: gameTick / 1000,
+        ease: Linear.easeNone
+      });
+    }
+  }, [gamePosition.x, gamePosition.y, gameTick, isDragging]);
 
   // return
 
   return (
     <div className="game">
       <Area>
-        {gamePosition && (
-          <div
-            className="game__square"
-            style={{
-              transform: `translate(${gamePosition.x}px, ${gamePosition.y}px)`,
-              transition: `transform ${gameTick}ms linear`
-            }}
-          />
-        )}
+        <div ref={$circle} className="game__circle" />
       </Area>
     </div>
   );
