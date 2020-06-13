720a801def96fb7809f9171691fe49eeef427f7a
Alexandre Massé <alexandre.masse94@gmail.com>
2020-06-13 19:18:10 +0200

    :recycle: move some logic from MainScene to Member

--- src/components/GamePhaser/Member.ts
+++ src/components/GamePhaser/Member.ts
@@ -1,11 +1,12 @@
 import * as Phaser from "phaser";
-import MainScene from "./MainScene";
+import MainScene, { CollisionCategories } from "./MainScene";
 import { enums } from "@colobobo/library";
 
 export default class Member extends Phaser.Physics.Matter.Image {
   private readonly _id: string;
   private _status: enums.member.Status = enums.member.Status.waiting;
   scene: MainScene;
+  private readonly _baseScale: number;
 
   constructor({
     scene,
@@ -13,7 +14,8 @@ export default class Member extends Phaser.Physics.Matter.Image {
     y = 0,
     texture,
     options,
-    id
+    id,
+    baseScale = 0.42
   }: {
     scene: MainScene;
     x?: number;
@@ -21,6 +23,7 @@ export default class Member extends Phaser.Physics.Matter.Image {
     texture: string;
     options?: Phaser.Types.Physics.Matter.MatterBodyConfig;
     id: string;
+    baseScale?: number;
   }) {
     super(scene.matter.world, x, y, texture, undefined, options);
 
@@ -29,17 +32,17 @@ export default class Member extends Phaser.Physics.Matter.Image {
     this.scene = scene;
 
     this._id = id;
+
+    this._baseScale = baseScale;
+
+    this.init();
   }
 
   // GETTERS & SETTERS
 
-  // id
-
   get id(): string {
     return this._id;
   }
-
-  // status
 
   get status(): enums.member.Status {
     return this._status;
@@ -47,5 +50,103 @@ export default class Member extends Phaser.Physics.Matter.Image {
 
   set status(value: enums.member.Status) {
     this._status = value;
+  }
+
+  // CUSTOM GETTERS
+
+  get baseHeight(): number {
+    return this._baseScale * this.height;
+  }
+
+  // FUNCTIONS
+
+  init() {
+    this.setScale(this._baseScale);
+    this.setAlpha(0);
+    this.setFixedRotation();
+    this.disableInteractive();
+    this.setCollisionCategory(
+      this.scene.collisionCategories[CollisionCategories.member]
+    );
+    // disable collision
+    this.setCollidesWith(0);
+  }
+
+  addEventListeners() {
+    // listen collision
+    // TODO : use https://github.com/mikewesthad/phaser-matter-collision-plugin
+  }
+
+  setPositionToStartPlatform() {
+    this.setX(this.scene.plateforms.start!.x);
+    this.setY(
+      this.scene.plateforms.start!.y -
+        this.scene.plateforms.start!.displayHeight / 2 -
+        this.baseHeight / 2
+    );
+  }
+
+  spawned() {
+    this.setPositionToStartPlatform();
+    this.setFixedRotation();
+
+    this.status = enums.member.Status.active;
+
+    this.scene.tweens.add({
+      alpha: 1,
+      targets: this,
+      scale: { from: 0.2, to: this._baseScale },
+      ease: "Sine.easeOut",
+      duration: 600,
+      onComplete: () => {
+        this.setCollidesWith([
+          this.scene.collisionCategories[CollisionCategories.default],
+          this.scene.collisionCategories[CollisionCategories.member],
+          this.scene.collisionCategories[CollisionCategories.platform],
+          this.scene.collisionCategories[CollisionCategories.wall]
+        ]);
+        this.setIgnoreGravity(false);
+        this.setInteractive();
+      }
+    });
+  }
+
+  trapped() {
+    this.setVelocity(0);
+    this.setIgnoreGravity(true);
+    this.setCollidesWith(0);
+    this.disableInteractive();
+    this.status = enums.member.Status.waiting;
+
+    // animate
+    this.scene.tweens.add({
+      targets: this,
+      alpha: 0,
+      scale: 0.2,
+      duration: 400,
+      ease: "Sine.easeIn"
+    });
+  }
+
+  arrived() {
+    this.disableInteractive();
+    this.setCollidesWith(0);
+    this.setIgnoreGravity(true);
+    this.setVelocity(0);
+    this.status = enums.member.Status.arrived;
+
+    // animate
+    this.scene.tweens.add({
+      targets: this,
+      alpha: 0,
+      x: this.scene.plateforms.finish!.x,
+      y:
+        this.scene.plateforms.finish!.y -
+        this.scene.plateforms.finish!.displayHeight / 2 -
+        this.displayHeight / 2,
+      scale: 0.2,
+      duration: 600,
+      ease: "Sine.easeIn"
+    });
   }
 }
