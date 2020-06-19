d67e09853fc55cfffa8611682cdf7a18affb48d5
Alexandre Massé <alexandre.masse94@gmail.com>
2020-06-19 04:39:25 +0200

    :bento: :wrench:  #112 add traps spritesheets & configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export { groups } from "./groups";
 export { membersConfig as members } from "./members";
 export { getPlatFormsConfig } from "./platforms";
+export { trapsConfigsByWorlds as traps } from "./traps";
 export { worldsConfig as worlds } from "./worlds";
